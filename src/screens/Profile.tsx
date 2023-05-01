d5102783f08aeb8014a1108073a94030128a6265
Bruno Santos <brunojsfla@gmail.com>
2023-05-01 10:50:11 -0300

    feat: implementando photo galery

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -5,6 +5,7 @@ import {
   Skeleton,
   Text,
   Heading,
+  useToast,
 } from "native-base";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -13,17 +14,59 @@ import { TouchableOpacity } from "react-native";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+
 const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(true);
+  const [image, setImage] = useState(undefined);
+  const toast = useToast();
+
+  async function handlePickImage() {
+    setPhotoIsLoading(true);
+
+    try {
+      let imageResult = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 4],
+        quality: 1,
+      });
+
+      if (imageResult.canceled) {
+        return;
+      }
+
+      if (imageResult.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          imageResult.assets[0].uri
+        );
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: "A imagem escolhida deve ter um tamanho menos que 5MB",
+            placement: "top",
+            bgColor: "red.500"
+          });
+        }
+
+        setImage(imageResult.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
-          {photoIsLoading ? (
+          {photoIsLoading && !image ? (
             <Skeleton
               w={PHOTO_SIZE}
               h={PHOTO_SIZE}
@@ -33,12 +76,12 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: "https://github.com/brunojsfla.png" }}
+              source={{ uri: image }}
               size={PHOTO_SIZE}
               alt="Foto do usuário"
             />
           )}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handlePickImage}>
             <Text
               color="green.500"
               fontWeight="bold"
